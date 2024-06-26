7fa7d918aa13b96f1df52c1e46476cd149de42ae
planethunter9 <inesmariapnascimento@gmail.com>
2024-06-26 17:32:18 +0100

    editar

--- app/src/main/java/com/example/prototipo/EditarLembreteActivity.kt
+++ app/src/main/java/com/example/prototipo/EditarLembreteActivity.kt
@@ -1,12 +1,9 @@
 package com.example.prototipo
 
 import android.app.Activity
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
 import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
-import android.text.InputType
 import android.view.Gravity
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
@@ -14,7 +11,6 @@ import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.core.view.setPadding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.util.Calendar
 
 class EditarLembreteActivity : Activity() {
 
@@ -38,15 +34,7 @@ class EditarLembreteActivity : Activity() {
 
         configureTopBar(mainLayout)
         configureForm(mainLayout)
-
-        val bottomNavContainer = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-        }
-
-        mainLayout.addView(bottomNavContainer)
-
-        configureBottomNavigationView(bottomNavContainer)
+        configureBottomNavigationView(mainLayout)
 
         oldCategoria = intent.getStringExtra("oldCategoria")
         oldLembrete = intent.getStringExtra("oldLembrete")
@@ -59,17 +47,8 @@ class EditarLembreteActivity : Activity() {
             volumeEditText.setText(lembreteParts[2])
         }
     }
-aaaaaaaaaaaa
+
     private fun configureTopBar(mainLayout: LinearLayout) {
-
-        val mainLayout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-            setPadding(32, 32, 32, 32)
-            setBackgroundColor(ContextCompat.getColor(context, android.R.color.white))
-        }
-        setContentView(mainLayout)
-
         val topBar = LinearLayout(this).apply {
             orientation = LinearLayout.HORIZONTAL
             layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
@@ -78,13 +57,27 @@ aaaaaaaaaaaa
 
         val typefaceBold = Typeface.createFromAsset(assets, "fonts/SF-Pro-Text-Bold.otf")
 
+        val voltarTextView = TextView(this).apply {
+            layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
+                marginEnd = 16
+            }
+            text = "Voltar"
+            textSize = 18f
+            setPadding(16)
+            typeface = typefaceBold
+            setTextColor(ContextCompat.getColor(context, android.R.color.holo_blue_light))
+            setOnClickListener {
+                finish()
+            }
+        }
+        topBar.addView(voltarTextView)
+
         val textView = TextView(this).apply {
             layoutParams = LinearLayout.LayoutParams(0, WRAP_CONTENT, 1f)
             text = "Editar Lembrete"
-            textSize = 28f
+            textSize = 22f
             setPadding(16)
             typeface = typefaceBold
-            setTextColor(ContextCompat.getColor(context, android.R.color.black))
             gravity = Gravity.CENTER
         }
         topBar.addView(textView)
@@ -92,88 +85,43 @@ aaaaaaaaaaaa
         mainLayout.addView(topBar)
     }
 
-
     private fun configureForm(mainLayout: LinearLayout) {
-
-        val typefaceRegular = Typeface.createFromAsset(assets, "fonts/SF-Pro-Display-Medium.otf")
-
-        val titleEditText = EditText(this).apply {
-            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-            hint = "Título"
-            inputType = InputType.TYPE_CLASS_TEXT
-            setPadding(16)
-            typeface = typefaceRegular
-            setBackgroundResource(android.R.drawable.edit_text)
+        categoriaEditText = EditText(this).apply {
+            hint = "Categoria"
+            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+                setMargins(0, 0, 0, 16)
+            }
         }
-        mainLayout.addView(titleEditText)
+        mainLayout.addView(categoriaEditText)
 
-        val dateTimeEditText = EditText(this).apply {
-            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-            hint = "Dia e hora"
-            inputType = InputType.TYPE_NULL
-            setPadding(16)
-            typeface = typefaceRegular
-            setBackgroundResource(android.R.drawable.edit_text)
-            setOnClickListener {
-                showDateTimePickerDialog(this)
+        lembreteEditText = EditText(this).apply {
+            hint = "Lembrete"
+            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+                setMargins(0, 0, 0, 16)
+            }
+        }
+        mainLayout.addView(lembreteEditText)
+
+        dateTimeEditText = EditText(this).apply {
+            hint = "Data e Hora (dd/MM/yyyy HH:mm)"
+            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+                setMargins(0, 0, 0, 16)
             }
         }
         mainLayout.addView(dateTimeEditText)
 
-        val volumeTextView = TextView(this).apply {
-            layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-            text = "Volume"
-            setPadding(0, 16, 0, 8)
-            typeface = typefaceRegular
-        }
-        mainLayout.addView(volumeTextView)
-
-        val volumeSpinner = Spinner(this).apply {
-            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-            adapter = ArrayAdapter.createFromResource(
-                context,
-                R.array.volume_array,
-                android.R.layout.simple_spinner_item
-            ).apply {
-                setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            }
-        }
-        mainLayout.addView(volumeSpinner)
-
-        val categoryTextView = TextView(this).apply {
-            layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-            text = "Categoria"
-            setPadding(0, 16, 0, 8)
-            typeface = typefaceRegular
-        }
-        mainLayout.addView(categoryTextView)
-
-        val categorySpinner = Spinner(this).apply {
-            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-            adapter = ArrayAdapter.createFromResource(
-                context,
-                R.array.categoria_array,
-                android.R.layout.simple_spinner_item
-            ).apply {
-                setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            }
-        }
-        mainLayout.addView(categorySpinner)
-
-        val buttonLayout = LinearLayout(this).apply {
+        volumeEditText = EditText(this).apply {
+            hint = "Volume"
             layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
-                topMargin = 32
+                setMargins(0, 0, 0, 16)
             }
-            orientation = LinearLayout.HORIZONTAL
-            gravity = Gravity.CENTER_HORIZONTAL
-            weightSum = 2f
         }
-        mainLayout.addView(buttonLayout)
+        mainLayout.addView(volumeEditText)
 
         val saveButton = Button(this).apply {
-            text = "Guardar"
-            layoutParams = LinearLayout.LayoutParams(0, WRAP_CONTENT, 1f).apply {
-                marginEnd = 16
+            text = "Salvar"
+            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+                setMargins(0, 32, 0, 0)
             }
             setOnClickListener {
                 val resultIntent = Intent().apply {
@@ -187,40 +135,6 @@ aaaaaaaaaaaa
             }
         }
         mainLayout.addView(saveButton)
-
-
-
-        val cancelButton = Button(this).apply {
-            text= "Cancelar"
-            layoutParams = LinearLayout.LayoutParams(0, WRAP_CONTENT, 1f)
-            setBackgroundColor(ContextCompat.getColor(context, android.R.color.darker_gray))
-            setTextColor(ContextCompat.getColor(context, android.R.color.white))
-            typeface= typefaceRegular
-            setPadding(16)
-            setOnClickListener {
-                finish()
-            }
-        }
-        buttonLayout.addView(cancelButton)
-    }
-
-    private fun showDateTimePickerDialog(editText: EditText) {
-        val cal = Calendar.getInstance()
-        val year = cal.get(Calendar.YEAR)
-        val month = cal.get(Calendar.MONTH)
-        val day = cal.get(Calendar.DAY_OF_MONTH)
-        val hour = cal.get(Calendar.HOUR_OF_DAY)
-        val minute = cal.get(Calendar.MINUTE)
-
-        val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
-            val timePickerDialog = TimePickerDialog(this, { _, selectedHour, selectedMinute ->
-                val selectedDateTime = String.format("%02d/%02d/%04d %02d:%02d",
-                    selectedDay, selectedMonth + 1, selectedYear, selectedHour, selectedMinute)
-                editText.setText(selectedDateTime)
-            }, hour, minute, true)
-            timePickerDialog.show()
-        }, year, month, day)
-        datePickerDialog.show()
     }
 
     private fun configureBottomNavigationView(mainLayout: LinearLayout) {
